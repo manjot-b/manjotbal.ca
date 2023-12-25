251ff93e025575a9e2dee68e187a604c190b25c1
Manjot Bal <manjot@manjotbal.ca>
2023-12-25 00:35:15 -0700

    Add Blog to nav bar.
    Needed to fix css so that smaller mobiles devices still display. Looks
    fine at >=280px width.

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ from jinja2 import Environment, FileSystemLoader
 # Update the build version whenever CSS or JS is updated.
 # This will ensure that the user will get the latest styling
 # rather than the locally cached version.
-BUILD_VERSION = '1.3.0'
+BUILD_VERSION = '1.3.1'
 
 parser = argparse.ArgumentParser(description='Build the jinja templates into a static html site.')
 parser.add_argument('--release',\
