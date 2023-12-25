145a1d71f351da5920b47ab6b765a1b55c589114
Manjot Bal <manjot@manjotbal.ca>
2023-12-24 19:49:15 -0700

    Store projects in a YAML file
    Cleans up the project HTML page. Now there is a for-loop to goes through
    all the categories and projects to build up the HTML page.
    
    Also, set site_url and build_version as global variables because there
    are used for all templates when rendering.

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ import os
 import rcssmin
 import shutil
 import subprocess
+import yaml
 from subprocess import Popen, PIPE
 from jinja2 import Environment, FileSystemLoader
 
@@ -29,9 +30,11 @@ args = parser.parse_args()
 template_dir = 'templates'
 content_dir = 'content'
 assets_dir = f'{content_dir}/assets'
+css_dir = f'{content_dir}/css'
+metadata_dir = 'metadata'
+
 dev_output_dir = 'output/dev'
 release_output_dir = 'output/release'
-css_dir = 'css'
 
 if args.release and not args.host:
     # Must be https
@@ -48,10 +51,22 @@ else:
     exit(1)
 
 env = Environment(loader=FileSystemLoader([template_dir, content_dir]))
+env.globals['site_url']=site_url
+env.globals['build_version']=BUILD_VERSION
 env.trim_blocks = True
 env.lstrip_blocks = True
 
 os.makedirs(output_dir, exist_ok=True)
+
+metadata = dict()
+yaml_files = [ f'{metadata_dir}/projects.yaml' ]
+for yaml_file in yaml_files:
+    yaml_template = env.get_template(yaml_file)
+    yaml_content = yaml.load(yaml_template.render(), yaml.Loader)
+
+    filename = yaml_file.split('/')[-1]
+    filename_key = filename.split('.')[0]
+    metadata[filename_key] = yaml_content
 
 for html_file in glob.iglob(f'{content_dir}/**/*.html', recursive=True):
     # No need to have the content directory appended to filename
@@ -67,10 +82,10 @@ for html_file in glob.iglob(f'{content_dir}/**/*.html', recursive=True):
         os.makedirs(f'{output_dir}/{path_to_html}', exist_ok=True)
 
     with open(f'{output_dir}/{html_file}', 'w') as outfile:
-        outfile.write(template.render(site_url=site_url, build_version=BUILD_VERSION))
+        outfile.write(template.render(metadata=metadata))
 
 # Make sure the less npm module is installed globally.
-for less_file in glob.iglob(f'{content_dir}/{css_dir}/*.less'):
+for less_file in glob.iglob(f'{css_dir}/*.less'):
     less_file = less_file[len(content_dir) + 1:]
     template = env.get_template(less_file)
     filename = less_file.split(".")[0]
@@ -78,12 +93,12 @@ for less_file in glob.iglob(f'{content_dir}/{css_dir}/*.less'):
     css_min = f'{filename}-min.css'
 
     proc = Popen(['lessc', '-', f'{output_dir}/{css}'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    proc.communicate(input=template.render(site_url=site_url).encode())
+    proc.communicate(input=template.render().encode())
 
     with open(f'{output_dir}/{css}', 'r') as css_file:
         minified = rcssmin.cssmin(css_file.read())
         with open(f'{output_dir}/{css_min}', 'w') as css_min_file:
-            css_min_file.write(minified)
+            css_min_file.write(str(minified))
 
 shutil.copytree(assets_dir, output_dir, dirs_exist_ok=True)
 
