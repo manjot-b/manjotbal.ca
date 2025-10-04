11e11c22f05a9cb976cb0796a0369c32b74918e6
Manjot Bal <manjot@manjotbal.ca>
2025-10-04 15:19:27 -0600

    Dockerize into nginx alpine container.
    Enabled gzip_static on nginx and perform gzip compression on rendered
    html and css files.
    
    Removed deploy from build.py because server will pull new Docker image
    when needed.

--- build.py
+++ build.py
@@ -1,29 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import argparse
 import glob
+import gzip
 import os
+import minify_html
 import rcssmin
 import shutil
-import subprocess
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
 # Update the build version whenever CSS or JS is updated.
 # This will ensure that the user will get the latest styling
 # rather than the locally cached version.
-BUILD_VERSION = '1.3.3'
+BUILD_VERSION = '2'
 
 parser = argparse.ArgumentParser(description='Build the jinja templates into a static html site.')
-parser.add_argument('--release',\
-        help='Uses the appropriate variables and paths for the release version.',\
+parser.add_argument('--no-min',\
+        help='Do not minify the html and css files. Used for debugging.',\
         action='store_true')
-parser.add_argument('--publish',\
-        help='Publish the website via rsync as USER. For example, this will publish to user@hostname. \
-        Publishes to the release server if --release is specified, otherwise --host needs to also be specified.',\
-        metavar='USER')
-parser.add_argument('--host', help='The hostname when building or publishing to dev server. \
-        e.g root@host. This must be provided if building or publishing the dev version.')
 args = parser.parse_args()
 
 template_dir = 'templates'
@@ -31,21 +26,7 @@ content_dir = 'content'
 assets_dir = f'{content_dir}/assets'
 css_dir = f'{content_dir}/css'
 metadata_dir = 'metadata'
-
-dev_output_dir = 'output/dev'
-release_output_dir = 'output/release'
-
-if args.release and not args.host:
-    # Must be https
-    hostname = 'manjotbal.ca'
-    output_dir = release_output_dir
-elif not args.release and args.host:
-    # Make sure nginx is up and running.
-    hostname = args.host
-    output_dir = dev_output_dir
-else:
-    print('Error: One of --release or --host must be specified.')
-    exit(1)
+output_dir = 'output'
 
 env = Environment(loader=FileSystemLoader([template_dir, content_dir]))
 env.globals['build_version']=BUILD_VERSION
@@ -75,17 +56,25 @@ for html_file in glob.iglob(f'{content_dir}/**/*.html', recursive=True):
     # No need to have the content directory appended to filename
     # because we already set the jinja environment to search in the
     # content directory.
-    html_file = html_file[len(content_dir) + 1:]
-    template = env.get_template(html_file)
+    html_file_path = html_file[len(content_dir) + 1:]
+    template = env.get_template(html_file_path)
 
     # The path to the html needs to exist before we can open the file.
-    paths = html_file.split('/')
+    paths = html_file_path.split('/')
     if len(paths) > 1:
         path_to_html = '/'.join(paths[0:-1])
         os.makedirs(f'{output_dir}/{path_to_html}', exist_ok=True)
 
-    with open(f'{output_dir}/{html_file}', 'w') as outfile:
-        outfile.write(template.render(metadata=metadata))
+    rendered_html = template.render(metadata=metadata)
+    with open(f'{output_dir}/{html_file_path}', 'w') as html_file:
+        if args.no_min:
+            html_file.write(rendered_html)
+        else:
+            html_min_file = minify_html.minify(rendered_html)
+            html_file.write(html_min_file)
+
+    with gzip.open(f'{output_dir}/{html_file_path}.gz', 'wb') as html_gzip_file:
+        html_gzip_file.write(rendered_html.encode('utf-8'))
 
 # Minify CSS files
 for css_file in glob.iglob(f'{css_dir}/*.css'):
@@ -96,17 +85,18 @@ for css_file in glob.iglob(f'{css_dir}/*.css'):
     css_min = f'{filename}-min.css'
     os.makedirs(f'{output_dir}/{os.path.dirname(css_min)}', exist_ok=True)
 
-    with open(f'{output_dir}/{css_min}', 'w') as css_min_file:
-        minified = rcssmin.cssmin(template.render())
-        css_min_file.write(str(minified))
+    rendered_css = template.render()
+    with open(f'{output_dir}/{css_min}', 'w') as css_file:
+        if args.no_min:
+            css_file.write(rendered_css)
+        else:
+            css_min_file = rcssmin.cssmin(rendered_css)
+            css_file.write(str(css_min_file))
+
+    with gzip.open(f'{output_dir}/{css_min}.gz', 'wb') as css_gzip_file:
+        css_gzip_file.write(rendered_css.encode('utf-8'))
 
 shutil.copytree(assets_dir, output_dir, dirs_exist_ok=True)
 
 print(f'Files generated in {output_dir}/')
-
-
-# Use rsync to publish the contents of the release folder to the sever.
-if args.publish:
-    subprocess.run(['rsync', '-azP', '--usermap=*:www-data', '--groupmap=*:www-data', \
-        f'{output_dir}/', f'{args.publish}@{hostname}:/var/www/manjotbal.ca'])
 
